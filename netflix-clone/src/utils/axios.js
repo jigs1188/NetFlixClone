c54969b91ee53af34dca52a0262b9e245e74f6c7
jigs1188 <103303890+jigs1188@users.noreply.github.com>
2025-05-01 03:04:39 +0530

    deployement frontend

--- netflix-clone/src/utils/axios.js
+++ netflix-clone/src/utils/axios.js
@@ -1,8 +1,15 @@
 // src/utils/axios.js
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Ensure this matches your backend URL
+
+
+export default axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-export default instance;
+
+// const instance = axios.create({
+//   baseURL: 'http://localhost:5000/api', // Ensure this matches your backend URL
+// });
+
+// export default instance;
